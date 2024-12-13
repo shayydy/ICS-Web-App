11f981dabd92a339d50f4adf3f3ca36c3021901f
shayydy <nightoftheshay@yahoo.com>
2024-12-12 20:32:14 -0500

    Update run.py
    I dont actually think we need this run file. i could be wrong so i'm just commenting out for now. 
    
    
    Signed-off-by: shayydy <nightoftheshay@yahoo.com>

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
-from project.main import main
+# from project.main import main
 
 
-if __name__ == "__main__":
+# if __name__ == "__main__":
     main.run(debug=True)
+
+If you set this up correctly, it should run from just entering "flask --app project run" even without this file
