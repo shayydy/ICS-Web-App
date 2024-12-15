57f0376ccecf8eb3d213f4752f22262404514e2d
shayydy <nightoftheshay@yahoo.com>
2024-12-15 12:32:54 -0500

    Delete run.py
    got rid of extraneous run file
    
    Signed-off-by: shayydy <nightoftheshay@yahoo.com>

--- run.py
+++
@@ -1,7 +0,0 @@
-# from project.main import main
-
-
-# if __name__ == "__main__":
-    main.run(debug=True)
-
-If you set this up correctly, it should run from just entering "flask --app project run" even without this file
