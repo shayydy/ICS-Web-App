9f43e88dcdbdf424382703decd8ac052b87f91f2
shayydy <nightoftheshay@yahoo.com>
2024-12-15 13:48:04 -0500

    Update __init__.py
    This cleaned up the code a bit and removed the create_app function, which is now in the app factory. 
    
    Signed-off-by: shayydy <nightoftheshay@yahoo.com>

--- __init__.py
+++ __init__.py
@@ -2,46 +2,35 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from .app_factory import create_app
+from .models import User
+from flask_bcrypt import Bcrypt
 import os
 
-# init SQLAlchemy so we can use it later in our models
-db = SQLAlchemy()
-loginmanager = LoginManager()
+# Create the Flask app instance
+app = Flask(__name__)
 
+# Configure the app
+app.config['SECRET_KEY'] = '1b9fac1236cc63bc46649867a4b3dd8d'  # Replace with a strong secret key
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///accounts.db'
 
+# Initialize extensions
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'auth.login'
 
-app = create_app()
+# User loader for Flask-Login
+@login_manager.user_loader
+def load_user(user_id):
+    return db.session.execute(db.select(User).filter_by(id=1)).scalar()
 
-def create_app():
-    app = Flask(__name__)
+# Register blueprints
+from .auth import auth as auth_blueprint
+app.register_blueprint(auth_blueprint)
 
-    app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+from .main import main as main_blueprint
+app.register_blueprint(main_blueprint)
 
-    db.init_app(app)
-
-    # blueprint for auth routes in our app
-    from .auth import auth as auth_blueprint
-    app.register_blueprint(auth_blueprint)
-
-    # blueprint for non-auth parts of app
-    from .main import main as main_blueprint
-
-    login_manager = LoginManager()
-    login_manager.login_view = 'auth.login'
-    login_manager.init_app(app)
-
-    from .models import User
-
-    @login_manager.user_loader
-    def load_user(user_id):
-		# since the user_id is just the primary key of our user table, 
-        # use it in the query for the user
-        return User.query.get(int(user_id))
-
-    with app.app_context():
-        db.create_all()
-
-    return app
-
-from . import routes
+# Create database tables if they don't exist
+with app.app_context():
+    db.create_all()
