ed7e3172a95e44367a0ffa135adf6a79f99887ba
shayydy <nightoftheshay@yahoo.com>
2024-12-15 12:28:06 -0500

    Update models.py
    added class Post info
    
    Signed-off-by: shayydy <nightoftheshay@yahoo.com>

--- models.py
+++ models.py
@@ -1,3 +1,5 @@
+from flask_login import UserMixin
+from datetime import datetime
 from . import db
 
 
@@ -7,3 +9,17 @@ class User(db.Model):
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
