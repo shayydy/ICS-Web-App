f7a0a51c95f601545f059129205485a20c5e4f7b
shayydy <nightoftheshay@yahoo.com>
2024-12-15 12:05:57 -0500

    Update main.py
    removed routes which are now in routes.py
    I odn't know that i need this file anymore. 
    
    Signed-off-by: shayydy <nightoftheshay@yahoo.com>

--- main.py
+++ main.py
@@ -8,36 +8,4 @@ from . import db
 main = Blueprint('main', __name__,
                         template_folder='templates')
 
-@main.route('/', defaults={'page': 'home'})
-@main.route('/home')
-def show(page):
-    try:
-        return render_template(f'main/{page}.html')
-    except TemplateNotFound:
-        return render_template('404.html'), 404
-
-
-@main.route('/cloud')
-@login_required
-def cservices():
-    return render_template('cloud.html')
-
-
-@main.route('/ics')
-@login_required
-def ics():
-    return render_template('ics.html')
-
-
-@main.route('/about')
-def about():
-    return render_template('about.html')
-
-
-@main.route('/profile')
-@login_required
-def profile():
-    return render_template('profile.html', name=current_user.name)
-
-
 migrate = Migrate(main, db)
